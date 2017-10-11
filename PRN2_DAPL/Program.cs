f695abea64add1f57d4442bb26fde97218eabbd2
diego-d5000 <diego-d@outlook.com>
2017-10-11 02:00:58 -0500

    added Persona, PruebaPersona and Libro classes to demostrate encapsulation in C#

---
+++ PRN2_DAPL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRN2_DAPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PRN2_DAPL/PruebaPersona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRN2_DAPL
+{
+    class PruebaPersona
+    {
+        static void Main(string[] args)
+        {
+            // Se crean dos objetos de tipo persona
+            Persona personaUno = new Persona();
+            Persona personaDos = new Persona();
+
+            // Se les asigna un nombre a cada uno
+            personaUno.SetNombre("Juan");
+            personaDos.SetNombre("Pedro");
+
+            // Se les pide que saluden
+            personaUno.Saludar();
+            personaDos.Saludar();
+
+            // para prevenir el cierre de la ventana
+            Console.ReadLine();
+        }
+    }
+}
